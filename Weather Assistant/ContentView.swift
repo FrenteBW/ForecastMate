db5d9bd4dcaf3280f3d4c78e389c3b8523c033a5
An Byungwook <abw2619@icloud.com>
2022-11-29 22:45:05 +0900

    Initial Commit

---
+++ Weather Assistant/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Weather Assistant
+//
+//  Created by 안병욱의 mac on 2022/11/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather Assistant/Weather_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AssistantApp.swift
+//  Weather Assistant
+//
+//  Created by 안병욱의 mac on 2022/11/29.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
